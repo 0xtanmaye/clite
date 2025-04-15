6127b3146e4ff7f2387a5fce16f33346a3c4a13d
0xtanmaye <106451224+0xtanmaye@users.noreply.github.com>
2025-04-15 13:18:05 +0530

    Enable cursor movement with WASD keys (w: up, s: down, a: left, d: right)

--- clite.cpp
+++ clite.cpp
@@ -258,6 +258,23 @@ void editorRefreshScreen()
 
 /*** input ***/
 
+void editorMoveCursor(char key) {
+	switch (key) {
+		case 'a':
+			E.cx--;
+			break;
+		case 'd':
+			E.cx++;
+			break;
+		case 'w':
+			E.cy--;
+			break;
+		case 's':
+			E.cy++;
+			break;
+	}
+}
+
 // Wait for a keypress and handle it
 void editorProcessKeypress()
 {
@@ -270,6 +287,13 @@ void editorProcessKeypress()
 			write(STDOUT_FILENO, "\x1b[2J", 4);
 			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
+			break;
+
+		case 'w':
+		case 'a':
+		case 's':
+		case 'd':
+			editorMoveCursor(c);
 			break;
 	}
 }
