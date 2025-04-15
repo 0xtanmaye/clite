bd81bc0b7b106dd742bd383fdc29b7631c38f8b8
0xtanmaye <106451224+0xtanmaye@users.noreply.github.com>
2025-04-15 21:25:32 +0530

    Implement page scrolling by moving cursor to top/bottom & simulating keypresses

--- clite.cpp
+++ clite.cpp
@@ -565,9 +565,16 @@ void editorProcessKeypress()
 
 		case PAGE_UP:
 		case PAGE_DOWN:
-			// For now make PAGE UP/DOWN move the cursor to the top/bottom of the screen
 			// The code is written in braces to allow creation of times variable
 			{
+				// Scroll page by moving cursor to top/bottom and simulating keypresses
+				if (c == PAGE_UP) {
+					E.cy = E.rowoff;
+				} else if (c == PAGE_DOWN) {
+					E.cy = E.rowoff + E.screenrows - 1;
+					if (E.cy > E.numrows) E.cy = E.numrows;
+				}
+
 				int times = E.screenrows;
 				while (times--)
 					editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
